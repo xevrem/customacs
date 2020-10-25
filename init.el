9239a93f79a4b0353bb537e0cc529fa1490e8987
erika jonell <erika.jonell@gmail.com>
2020-10-25 12:30:59 -0400

    minor tab adjustment

--- init.el
+++ init.el
@@ -16,8 +16,8 @@
 ;; init srouces
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
