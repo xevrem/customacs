785fed31fabd1f9672f85c81b1f6b1f55f0e4d67
erika jonell <erika.jonell@gmail.com>
2021-05-24 07:07:08 -0400

    updated themes

--- init.el
+++ init.el
@@ -330,8 +330,10 @@
   (setq doom-themes-enable-bold t
         doom-themes-enable-italics t)
   ;; (load-theme 'doom-snazzy t)
-  (load-theme 'doom-tomorrow-night t)
+  ;; (load-theme 'doom-tomorrow-night t)
+  (consult-theme 'doom-snazzy)
   )
+
 
 ;; make it easier to keep track of parens and braces
 (use-package rainbow-delimiters
@@ -1249,16 +1251,3 @@
   (setq gcmh-high-cons-threashold (* 1024 1024 100)
         gcmh-idle-delay 60)
   )
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("7b3d184d2955990e4df1162aeff6bfb4e1c3e822368f0359e15e2974235d9fa8" default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
