33eb4fb162d314184f13c6dac04dedeeaef1f77a
erika jonell <erika.jonell@tenet3.com>
2022-07-18 15:36:06 -0400

    fix default straight.el github string

--- init.el
+++ init.el
@@ -179,7 +179,7 @@
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         "https://raw.githubusercontent.com/radian-software/straight.el/master/install.el"
          'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)
