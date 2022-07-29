383baad9a9fd77ebdb47122cce7d9a457b8d38a8
erika jonell <erika.jonell@tenet3.com>
2022-07-29 13:18:09 -0400

    sizing

--- init.el
+++ init.el
@@ -21,11 +21,11 @@
               ;; disable until we actually call it
               recentf-auto-cleanup 'never
               ;; adjust the startup size of emacs
-              default-frame-alist `((width . 137) ;; chars
-                                    (height . 35) ;; lines
+              default-frame-alist `((width . 120) ;; chars
+                                    (height . 40) ;; lines
                                     )
-              initial-frame-alist `((width . 137) ;; chars
-                                    (height . 35) ;; lines
+              initial-frame-alist `((width . 120) ;; chars
+                                    (height . 40) ;; lines
                                     )
               ;; Resizing the Emacs frame can be a terribly expensive part of changing the
               ;; font. By inhibiting this, we halve startup times, particularly when we use
@@ -136,8 +136,8 @@
       (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
       )
     ;; set current frame to 120x45 characters
-    (set-frame-width (frame-focus) 147)
-    (set-frame-height (frame-focus) 37)
+    (set-frame-width (frame-focus) 120)
+    (set-frame-height (frame-focus) 40)
     (doom-modeline-refresh-font-width-cache)
     )
   )
